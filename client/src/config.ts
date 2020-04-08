98e676538a2da36be4cc849e0687c19ad873348c
Satish Kumar <s.n.kumar.satish.sk@gmail.com>
2020-04-08 11:21:59 +0530

    frontend framework added

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = ''  // UPDATE API ID
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-0dc6kjik.auth0.com',    // UPDATE AUTH DOMAIN
+  clientId: 'S1O7Pg5GA1Fdj9Rhw5CZPI13vAEAloBo',  // UPDATE AUTH CLIENT ID
+  callbackUrl: 'http://localhost:3000/callback'
+}
